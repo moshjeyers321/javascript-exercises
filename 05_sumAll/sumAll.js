7c2112cd2fd663b937ca7ceaf1f8ad88f8b78d96
Josh <pianoman04428@gmail.com>
2021-11-09 15:18:58 -0600

    finished 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(...nums) {
+    const negative = (el) => el < 0;
 
+
+    if (nums.some(negative) || nums.some(el => typeof el !== 'number')){
+        return "ERROR"
+    }
+    let max = Math.max(...nums);
+    let min = Math.min(...nums);
+
+    let sum = 0;
+
+    for (let i = min; i <=max; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
