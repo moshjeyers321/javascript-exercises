dc168760b58fc542150f5e778090d107d0a1faeb
Josh <pianoman04428@gmail.com>
2021-11-09 14:47:40 -0600

    finished 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        if (args.includes(element)){
+            continue;
+        }
+        newArr.push(element);
+    }
+    return newArr;
 };
 
 // Do not edit below this line
